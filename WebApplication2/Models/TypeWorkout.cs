572af6e64d8d87ff9f50db6c0645b779b7394153
Alex Fedeles <alexfede03@yahoo.com>
2019-08-06 17:53:16 +0300

    add the project
    add the project

---
+++ WebApplication2/Models/TypeWorkout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class TypeWorkout
+    {
+        public int TypeWorkoutID { get; set; }
+        public int Repetition { get; set; }
+        public int Serie { get; set; }
+        public float Pause { get; set; }
+        public string Description { get; set; }
+
+        public virtual int WorkoutID { get; set; }
+        public virtual int ExerciseID { get; set; }
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
